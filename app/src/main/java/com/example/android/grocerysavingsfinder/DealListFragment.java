17157730181ed8ca063b69540b9af7270318d721
Tybor2 <tborden14@yahoo.com>
2019-04-24 17:02:24 -0400

    Added list item dividers

--- app/src/main/java/com/example/android/grocerysavingsfinder/DealListFragment.java
+++ app/src/main/java/com/example/android/grocerysavingsfinder/DealListFragment.java
@@ -8,6 +8,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
@@ -53,6 +54,9 @@ public class DealListFragment extends Fragment {
         mDealRecyclerView = (RecyclerView) view
                 .findViewById(R.id.deal_recycler_view);
         mDealRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        RecyclerView.ItemDecoration itemDecoration = new
+                DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL);
+        mDealRecyclerView.addItemDecoration(itemDecoration);
 
         updateUI();
 
