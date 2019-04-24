a1a0ca6b3cf156ee03505098ba815d3dafdd6a74
Tybor2 <tborden14@yahoo.com>
2019-04-24 15:35:37 -0400

    Updated database

--- app/src/main/java/com/example/android/grocerysavingsfinder/database/DealCollectionHelper.java
+++ app/src/main/java/com/example/android/grocerysavingsfinder/database/DealCollectionHelper.java
@@ -3,11 +3,12 @@ package com.example.android.grocerysavingsfinder.database;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 import com.example.android.grocerysavingsfinder.database.DealDbSchema.DealTable;
 
 public class DealCollectionHelper extends SQLiteOpenHelper {
-    private static final int VERSION = 1;
+    private static final int VERSION = 2;
     private static final String DATABASE_NAME = "dealcollection.db";
 
     public DealCollectionHelper(Context context) {
@@ -32,6 +33,11 @@ public class DealCollectionHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.e("DealCollectionHelper", "Updating table from " + oldVersion + " to " + newVersion);
+        if(oldVersion < 2) {
+            db.execSQL("DROP TABLE Deals");
 
+        }
+        onCreate(db);
     }
 }
